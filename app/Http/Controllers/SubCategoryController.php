f290756d4b73abdd7f7dbabaa71a6037ff1bbdd3
Mostafadarsh <119732135+mostafahassan2001@users.noreply.github.com>
2025-07-20 17:04:30 +0300

    modificaion data sub_category

--- app/Http/Controllers/SubCategoryController.php
+++ app/Http/Controllers/SubCategoryController.php
@@ -4,22 +4,7 @@ namespace App\Http\Controllers;
 use App\Models\SubCategory;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
-/**
- * @OA\Schema(
- *     schema="SubCategory",
- *     type="object",
- *     title="SubCategory",
- *     required={"id", "name_en", "name_ar", "category_id"},
- *     @OA\Property(property="id", type="integer", example=1),
- *     @OA\Property(property="name_en", type="string", example="Shirts"),
- *     @OA\Property(property="name_ar", type="string", example="قمصان"),
- *     @OA\Property(property="description_en", type="string", example="Subcategory for men's shirts"),
- *     @OA\Property(property="description_ar", type="string", example="قسم فرعي للقمصان الرجالية"),
- *     @OA\Property(property="category_id", type="integer", example=2),
- *     @OA\Property(property="status", type="boolean", example=true),
- *     @OA\Property(property="logo", type="string", example="SubCategory/logo.jpg")
- * )
- */
+
 class SubCategoryController extends Controller
 {
       /**
@@ -35,7 +20,7 @@ class SubCategoryController extends Controller
      *             @OA\Property(property="pageSize", type="integer", example=10),
      *             @OA\Property(property="totalPageNumber", type="integer", example=5),
      *             @OA\Property(property="data", type="array",
-     *                 @OA\Items(ref="#/components/schemas/Product")
+     *                 @OA\Items(ref="#/components/schemas/SubCategory")
      *             )
      *         )
      *     )
--- app/Models/SubCategory.php
+++ app/Models/SubCategory.php
@@ -3,7 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+/**
+ * @OA\Schema(
+ *     schema="SubCategory",
+ *     type="object",
+ *     title="SubCategory",
+ *     required={"id", "name_en", "name_ar", "category_id"},
+ *     @OA\Property(property="id", type="integer", example=1),
+ *     @OA\Property(property="name_en", type="string", example="Shirts"),
+ *     @OA\Property(property="name_ar", type="string", example="قمصان"),
+ *     @OA\Property(property="description_en", type="string", example="Subcategory for men's shirts"),
+ *     @OA\Property(property="description_ar", type="string", example="قسم فرعي للقمصان الرجالية"),
+ *     @OA\Property(property="category_id", type="integer", example=2),
+ *     @OA\Property(property="status", type="boolean", example=true),
+ *     @OA\Property(property="logo", type="string", example="SubCategory/logo.jpg")
+ * )
+ */
 class SubCategory extends Model
 {
     protected $fillable = [
